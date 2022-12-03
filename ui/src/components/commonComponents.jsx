cac34e02cf058e4dd353cee04df3af94cb10b27d
Subash R <waytosubash@gmail.com>
2022-12-03 11:02:51 +0530

    Adding header and footer

--- ui/src/components/commonComponents.jsx
+++ ui/src/components/commonComponents.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 
 function Header() {
-  return(
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Header
-      </h1>
-    </>
+  return (
+    <Card bgcolor="light" textcolor="text-red-400" label="Header" subtext="nothing" />
   );
 }
 
 function Footer() {
-  return(
-    <>
-      Footer
-    </>
+  return (
+    <Card bgcolor="light" textcolor="text-red-400" label="Footer" subtext="nothing" />
   );
 }
 
-export {Footer, Header};
+function Card(props) {
+  return (
+    <div className="mt-4">
+      <div className={`flex items-start rounded-xl bg-white p-4 shadow-lg ${props.bgcolor === 'light' ? 'bg-white' : 'bg-black' }`}>
+        <div className="ml-4">
+          <button className={`font-semibold ${props.textcolor}`}>{props.label}</button>
+          <p className="mt-2 text-sm text-gray-500">{props.subtext}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export {Footer, Header, Card};
